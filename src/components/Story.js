edd69059ae8e7315e28ec83730565793fa9634ff
shivang10 <shivang10garg@gmail.com>
2020-05-18 19:09:34 +0530

    data fetching and styling

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {getStory} from '../services/hnApi';
+import { StoryWrapper, StoryTitle, StoryMetaElement, StoryMeta } from '../styles/StoryStyles';
+import { mapTime } from '../mappers/mapTime';
 
 export const Story = ({storyId}) => {
     const [story, setStory] = useState({});
@@ -9,12 +11,19 @@ export const Story = ({storyId}) => {
     }, [])
 
     return story && story.url ? (
-        <>
-            <a href={story.url}>
-                <p>{story.title}</p>
-            </a>
-            By: <p>{story.by}</p>
-            Posted: <p>{story.time}</p>
-        </>
+        <StoryWrapper data-testid="story">
+            <StoryTitle>
+            <a href={story.url}>{story.title}</a>
+            </StoryTitle>
+            <StoryMeta>
+                <span data-testid="story-by">
+                    <StoryMetaElement color="#000">By: </StoryMetaElement>{story.by}
+                </span>
+                <span data-testid="story-time">
+                    <StoryMetaElement color="#000">Posted:</StoryMetaElement> {` `}
+                    {mapTime(story.time)}
+                </span>
+            </StoryMeta>
+        </StoryWrapper>
     ) : null;
-}
+};
--- src/containers/StoriesContainers.js
+++ src/containers/StoriesContainers.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
-import {getStoryIds, getStory} from '../services/hnApi'
+import {getStoryIds} from '../services/hnApi'
 import {Story} from '../components/Story';
-
+import { GlobalStyle, StoriesContainerWrapper} from '../styles/StoriesContainerStyles';
 
 export const StoriesContainer = () => {
 
@@ -9,13 +9,18 @@ export const StoriesContainer = () => {
   
   useEffect(() => {
     getStoryIds().then(data => setStoryIds(data));
-    getStory(23220847).then(
-        data => console.log(data)
-      )
   }, []);
 
   // when the component mounts, do this.... [storyidupdate (true/false)] in useeffect
-  return storyIds.map(storyId => (
-      <Story key={storyId} storyId={storyId} />
-  ));
+  return (
+  <>
+  <GlobalStyle />
+  <StoriesContainerWrapper data-test-d="stories-container">
+    <h1>Hacker News Stories</h1>
+      {storyIds.map(storyId => (
+        <Story key={storyId} storyId={storyId} />
+      ))}
+    </StoriesContainerWrapper>
+  </>
+  )
 }
